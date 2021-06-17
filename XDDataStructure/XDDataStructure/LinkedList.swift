7789e257112c2f4f78907a7c24cb8dd9c5126bac
许冬冬 <1401315607@qq.com>
2021-06-17 19:24:50 +0800

    单链表

--- XDDataStructure/XDDataStructure/LinkedList.swift
+++ XDDataStructure/XDDataStructure/LinkedList.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 单链表节点
-class SingleLinkedListNode <T> {
+class SingleLinkedListNode <T:Hashable> {
     var next: SingleLinkedListNode?
     var value:T
     init(value:T) {
@@ -17,86 +17,94 @@ class SingleLinkedListNode <T> {
 }
 
 /// 单链表
-class SingleLinnkedList<T> {
+class SingleLinnkedList<T:Hashable> {
     var head:SingleLinkedListNode<T>?
-    var size = 0
+    var count = 0
     
-    /// 判空
-    /// - Returns: 
+    /// 数量
+    /// - Returns:
+    func size() -> Int {
+        return count
+    }
+    
     func isEmpty() -> Bool {
         return head == nil
     }
     
-    /// 尾部添加节点
-    /// - Parameter value: 
-    func append(value:T) {
-        let newNode = SingleLinkedListNode(value: value)
-        guard var node = head else {
-            head = newNode
-            size += 1
-            return
+    /// 是否包含某元素
+    /// - Parameter ele:
+    /// - Returns:
+    func contains(ele:T) -> Bool {
+        var node = head
+        while let n = node?.next  {
+            if n.value == ele {
+                return true
+            }
+            node = n
         }
-        while let next = node.next {
-            node = next
-        }
-        node.next = newNode
-        size += 1
+        return false
     }
     
-    /// 获取某索引下节点
-    /// - Parameter atIndex: <#atIndex description#>
-    /// - Returns: <#description#>
-    func node(atIndex:Int) -> SingleLinkedListNode<T>? {
-        if atIndex < 0 || atIndex >  size-1 {
+    /// 尾部插入一节点
+    /// - Parameter ele: 
+    func add(ele:T) {
+        let newNode = SingleLinkedListNode(value: ele)
+        if head == nil {
+            head = newNode
+            count += 1
+            return
+        }
+        var node = head
+        while let n = node?.next {
+            node = n
+        }
+        node?.next = newNode
+        count += 1
+    }
+    
+    /// 获取某位置的元素
+    /// - Parameter index:
+    /// - Returns:
+    func get(index:Int) -> SingleLinkedListNode<T>? {
+        if index<0 || index >= count {
             return nil
         }
         var node = head
-        for _ in 0..<atIndex {
-            if node?.next == nil {
-                break;
-            }else {
-                node = node?.next
-            }
+        for _ in 0...index-1 {
+            node = node?.next
         }
+        
         return node
     }
     
-    /// 某索引插入节点
+    
+    /// 设置某位置的元素
     /// - Parameters:
-    ///   - n: <#n description#>
-    ///   - index: <#index description#>
-    func insert(n:SingleLinkedListNode<T>,index:Int) {
-        if index < 0 && index > size-1  {
-            return
+    ///   - index:
+    ///   - ele:
+    /// - Returns:
+    func set(index:Int,ele:T) -> SingleLinkedListNode<T>? {
+        if index < 0 || index >= count {
+            return nil
         }
+        let newNode = SingleLinkedListNode(value: ele)
         if index == 0 {
-            n.next = head
-            head = n
-        }else {
-            let pre = node(atIndex: index-1)
-            n.next = pre?.next
-            pre?.next = n
+            let pre = head
+            newNode.next = head?.next
+            head = newNode
+            return pre
         }
-        size += 1
-    }
-    
-    /// 移除所有节点
-    func removeAll() {
-        head = nil
-    }
-    
-    func remove(node:SingleLinkedListNode<T>) -> T? {
-        var n = head
-        while n != nil {
-            
-        }
-        return nil
+        var node = get(index: index-1)
+        let pre = node?.next
+        node?.next = newNode
+        
+        return pre
     }
     
     func description() -> String {
         var str = "LinkedList:"
         var node = head
-        for index in 0..<size {
+        for index in 0..<count {
             if node == nil {
                 break
             }
--- XDDataStructure/XDDataStructure/main.swift
+++ XDDataStructure/XDDataStructure/main.swift
@@ -7,21 +7,18 @@
 
 import Foundation
 
-var arr = ArrayList<Int>(capaticy: 20)
-arr.add(ele: 7)
-arr.add(ele: 9)
-arr.add(index: 0, ele: 99)
+//var arr = ArrayList<Int>(capaticy: 20)
+//arr.add(ele: 7)
+//arr.add(ele: 9)
+//arr.add(index: 0, ele: 99)
 //arr.add(ele: 9)
 //arr.add(ele: 3)
-print(arr.indexOf(ele: 7))
-print(arr.description())
+//print(arr.indexOf(ele: 7))
+//print(arr.description())
+
 
 //
-let linkedList = SingleLinnkedList<Int>()
-//linkedList.append(value: 7)
-//linkedList.append(value: 9)
-//let node = linkedList.node(atIndex: 0)
-//linkedList.insert(n: SingleLinkedListNode(value: 8), index: 9)
-//print(node?.value)
-linkedList.insert(n: SingleLinkedListNode(value: 9), index: 1)
+var linkedList = SingleLinnkedList<Int>()
+linkedList.add(ele: 8)
+print(linkedList.get(index: 0)?.value)
 print(linkedList.description())
