8348bdcdb599176f8a584530774a41d79f3a03a4
许冬冬 <1401315607@qq.com>
2021-06-29 21:32:48 +0800

    双向链表

--- XDDataStructure/XDDataStructure/LinkedList.swift
+++ XDDataStructure/XDDataStructure/LinkedList.swift
@@ -4,9 +4,23 @@
 //
 //  Created by Qiyeyun2 on 2021/5/28.
 //
-
+/**
+ 链表的遍历
+ 方法一：
+ var node = head
+ while node != nil {
+    node = node?.next
+ }
+ 方法二：
+ var node = head
+ for index in 0..<count {
+    //判断提前退出
+    xxxxx retrun
+    node = node.next
+ }
+ */
 import Foundation
-
+//MARK: - 单链表
 /// 单链表节点
 class SingleLinkedListNode <T:Hashable> {
     var next: SingleLinkedListNode?
@@ -45,6 +59,7 @@ class SingleLinnkedList<T:Hashable> {
             }
             node = n
         }
+        
         return false
     }
     
@@ -100,10 +115,7 @@ class SingleLinnkedList<T:Hashable> {
             return nil
         }
         var node = head
-        if atIndex == 0 {
-            return node
-        }
-        for _ in 1 ... atIndex {
+        for _ in 0 ..< atIndex {
             node = node?.next
         }
         return node
@@ -190,4 +202,162 @@ class SingleLinnkedList<T:Hashable> {
         return str
     }
     
+}
+
+//MARK: - 双向链表
+
+class DoubleLinkedListNode <T:Hashable> {
+    var next: DoubleLinkedListNode?
+    var prev: DoubleLinkedListNode?
+    var value:T
+    init(value:T) {
+        self.value = value
+    }
+}
+
+class DoubleLinkedList<T:Hashable> {
+    var head:DoubleLinkedListNode<T>?
+    var tail:DoubleLinkedListNode<T>?
+    var count = 0
+    
+    /// 数量
+    /// - Returns:
+    func size() -> Int {
+        return count
+    }
+    
+    func isEmpty() -> Bool {
+        return head == nil
+    }
+    
+    /// 是否包含某元素
+    /// - Parameter ele:
+    /// - Returns:
+    func contains(ele:T) -> Bool {
+        return indexOf(ele: ele) != -1
+    }
+    
+    /// 尾部插入一节点
+    /// - Parameter ele:
+    func add(ele:T) {
+        
+    }
+    
+    /// 获取某位置的元素
+    /// - Parameter index:
+    /// - Returns:
+    func get(index:Int) -> T? {
+        return node(atIndex: index)?.value
+    }
+    
+    
+    /// 设置某位置的元素
+    /// - Parameters:
+    ///   - index:
+    ///   - ele:
+    /// - Returns:
+    func set(index:Int,ele:T) -> T? {
+        let n = node(atIndex: index)
+        let old = n?.value
+        n?.value = ele
+        return old
+    }
+    
+    /// 获取某位置的节点
+    /// - Parameter atIndex:
+    /// - Returns:
+    func node(atIndex:Int) -> DoubleLinkedListNode<T>? {
+        if atIndex < 0 || atIndex >= count {
+            return nil
+        }
+        if atIndex < count >> 1 {
+            var node = head
+            for _ in 0..<atIndex {
+                node = node?.next
+            }
+            return node
+        }else {
+            var node = tail
+            for _ in atIndex+1 ..< count {
+                node = node?.prev
+            
+            }
+            return node
+        }
+    }
+    
+    /// 插入节点
+    /// - Parameters:
+    ///   - index:
+    ///   - ele:
+    func add(index:Int,ele:T) {
+//        if index < 0 || index>count {
+//            return
+//        }
+//        let newNode = SingleLinkedListNode(value: ele)
+//        if index == 0 {
+//            newNode.next = head
+//            head = newNode
+//            count += 1
+//            return
+//        }
+//        let n = node(atIndex: index-1)
+//        newNode.next = n?.next
+//        n?.next = newNode
+//        count += 1
+    }
+    
+    /// 删除某位置的节点
+    /// - Parameter index:
+    func remove(index:Int) {
+//        if index < 0 || index >= count {
+//            return
+//        }
+//        if index == 0 {
+//            head = head?.next
+//            count -= 1
+//            return
+//        }
+//        let n = node(atIndex: index-1)
+//        n?.next = n?.next?.next
+    }
+    
+    /// 获取某元素的位置
+    /// - Parameter ele:
+    /// - Returns:
+    func indexOf(ele:T) -> Int {
+        var node = head
+        for index in 0..<count {
+            if node?.value == ele {
+                return index
+            }
+            node = node?.next
+        }
+        return -1
+    }
+    
+    /// 清空链表
+    func clear() {
+        head = nil
+        tail = nil
+        count = 0
+    }
+    
+    func description() -> String {
+        var str = "LinkedList:"
+        var node = head
+        for index in 0..<count {
+            if node == nil {
+                break
+            }
+            if index == 0 {
+                str += "\(node!.value)"
+            }else {
+                str += " -> \(node!.value)"
+            }
+            node = node?.next
+        }
+        
+        return str
+    }
 }
